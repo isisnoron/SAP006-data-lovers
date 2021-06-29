edbe79fb5cd825537232abd9717d7186c4b6df78
Isisnoron <isisbeatrizrn@gmail.com>
2021-06-29 16:17:31 -0300

    primeiro commit com função ordenar

--- src/data.js
+++ src/data.js
@@ -1,9 +1,68 @@
-// estas funciones son de ejemplo
+export const orderGhibli = (data) => data.sort((a,z) => a.title > z.title ? 1 : -1);
+export const sortAZ = (data) => orderGhibli(data).slice(0, 20); //Retorna uma parte de um array existente
+export const sortZA = (data) => sortAZ(data).reverse();
 
-export const example = () => {
+//console.log("orderGhibli")
+
+/*export const orderGhibli = (database, condition) => {
+  if (!condition) return database;
+
+  if (condition === "az" || condition === "za") {
+    const orderAZ = database.sort(function (a, b) {
+      if (a.title < b.title) return -1;
+      if (a.title > b.title) return 1;
+      return 0;
+    });
+
+    if (condition === "az") return orderAZ;
+
+    return orderAZ.reverse();
+  }
+}/*
+
+
+
+
+/*export const filterData = (database, type, condition) => {
+  if (!condition) return database;
+  const filterResult = database.filter((item) => item[type] === condition);
+  return filterResult;
+}; 
+ console.log("filterData");   */
+
+
+/*export const genero = (data, condition) => {
+  const fem = data.results.filter(results => results.gender == condition)
+  console.log(fem);
+    return fem;
+};*/
+
+  //opção de filtrar por gênero
+/*
+respostaFem = []
+for (item of genero) {
+if (item === "Female")
+respostaFem.push(item)
+}
+
+respostaMas = []
+for (item of genero) {
+  if (item === "Male")
+  respostaMas.push(item)
+}
+console.log(respostaMas);
+*/
+
+/*export const example = () => {
   return 'example';
 };
 
 export const anotherExample = () => {
   return 'OMG';
-};
+};*/
+
+
+
+/*filterData(data, condition): esta função receberia os dados e nos retornaria os que cumprem com a condição.
+sortData(data, sortBy, sortOrder): esta função recebe três parâmetros. O primeiro, data, nos entrega os dados. O segundo, sortBy, diz respeito a qual das informações quer usar para ordenar. O terceiro, sortOrder, indica se quer ordenar de maneira crescente ou decrescente.
+computeStats(data): essa função nos permite fazer cálculos estatísticos básicos para serem exibidos de acordo com o que os dados permitem.  */
--- src/main.js
+++ src/main.js
@@ -1,6 +1,116 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import { orderGhibli, sortAZ, sortZA } from './data.js';
 
-console.log(example, data);
+import data from './data/ghibli/ghibli.js';
+
+console.log(orderGhibli)
+
+//const animation = data.films
+//console.log(orderGhibli, data);
+//console.log(genero, data);
+
+
+function printMovies(data) {
+  document.getElementById('card').innerHTML = data.map((film) => `
+  <div class="card">
+      <div class="front">
+      <div class="image" style="background-image: url(${film.poster})"></div>
+      <div class="text">
+      <h3 class="title">${film.title}</h3>
+      <h4 class="ano">${film.release_date} </h4>
+     </div> 
+    <div class="back">
+      <ul class="cardInfos">
+        <li><strong>Nome:</strong> ${film.title}</li>
+        <li><strong>Diretor: </strong>${film.director}</li>
+        <li><strong> Descrição:</strong> ${film.description}</li>
+      </ul>
+  </div>`)
+};
+
+printMovies(data.films)
+
+
+
+function ordenarAZ() {
+  const ordered = printMovies(sortAZ(data.films))
+  return ordered
+}
+const selectOrdenar = document.getElementById("order");
+selectOrdenar.addEventListener("change", ordenarAZ);
+
+function ordenarZA() {
+  const ordered2 = printMovies(sortZA(data.films))
+  return ordered2
+
+}
+const selectOrdenar2 = document.getElementById("za");
+selectOrdenar2.addEventListener("change", ordenarZA); 
+
+
+
+/*
+const animations = data.films
+const selectOrdenar = document.getElementById("order");
+
+function filterOrdenar(){
+    const valueSelected = selectOrdenar.value
+    const selectedMovies = orderGhibli(animations,valueSelected);
+    printMovies(selectedMovies);
+}
+
+
+selectOrdenar.addEventListener("change", filterOrdenar); */
+
+
+//const statisticInfo = document.getElementById("results");
+
+
+//declarei e atribui valor as variáveis 
+//const az = document.getElementById("az");
+/*const za = document.getElementById("za");
+const search = document.getElementById("search");
+const feminino = document.getElementById("feminino");
+const masculino = document.getElementById("masculino");*/
+
+
+/*az.onchange = function (evento) {
+  printMovies(data.films)
+  let crescente = sortAZ(data.title);
+  printMovies = (crescente);
+
+  evento.preventDefault();
+}; */
+
+
+/*const animations = data.films.map(filmes => filmes.title); //usei map para pegar todos os títulos
+console.log(animations)*/
+
+
+/*const respostaFem = data.films.filter(item => item.people)   //função que cria um novo array recebendo os item generos
+console.log(respostaFem)
+
+
+function ordenarAZ() {
+  const filtroOrdenar = document.getElementById("order").value
+  const ordenar = orderGhibli(data, animations)
+  console.log(filtroOrdenar)
+  document.getElementById("ordenar").addEventListener("change", ordenarAZ)
+  return ordenarAZ
+}
+
+*/
+
+/*window.onload = createCards(characters);
+
+document.getElementById("cards").innerHTML = printCards;
+
+createCards(data.title);
+console.log("createCards")*/
+
+/*const ordenarAZ = (evento) => {
+
+    evento.preventDefault(); 
+    //chamando a função de ordenar AZ
+};*/
+
+//filterData(data, gender)
