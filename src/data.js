b4c7baeb9c7e0a7bc52429feb4fdfc50add2406b
Isisnoron <isisbeatrizrn@gmail.com>
2021-07-05 16:21:01 -0300

    função filtro

--- src/data.js
+++ src/data.js
@@ -1,10 +1,16 @@
-export const orderGhibli = (data) => data.sort((a,z) => a.title > z.title ? 1 : -1);
+import data from "./data/ghibli/ghibli.js";
+const orderGhibli = (data) => data.sort((a, z) => a.title > z.title ? 1 : -1);
 export const sortAZ = (data) => orderGhibli(data).slice(0, 20); //Retorna uma parte de um array existente
 export const sortZA = (data) => sortAZ(data).reverse();
 
-//console.log("orderGhibli")
+export const films = data.films;
+export const people = films.reduce((total, film) => total.concat(film.people), []);
+console.log(people)
 
-/*export const orderGhibli = (database, condition) => {
+export const filtrar = (arr, key, value) =>
+  arr.filter((item) => item[key] === value);
+
+/*export const orderGhibli = (database, condition) => {    //primeira tentativa ordenar
   if (!condition) return database;
 
   if (condition === "az" || condition === "za") {
@@ -18,51 +24,27 @@ export const sortZA = (data) => sortAZ(data).reverse();
 
     return orderAZ.reverse();
   }
-}/*
+}*/
 
 
 
 
-/*export const filterData = (database, type, condition) => {
-  if (!condition) return database;
-  const filterResult = database.filter((item) => item[type] === condition);
-  return filterResult;
-}; 
- console.log("filterData");   */
-
-
-/*export const genero = (data, condition) => {
+/*export const genero = (data, condition) => {      //vídeo daniel, primeira tentativa filtragem
   const fem = data.results.filter(results => results.gender == condition)
   console.log(fem);
     return fem;
 };*/
 
-  //opção de filtrar por gênero
-/*
-respostaFem = []
-for (item of genero) {
-if (item === "Female")
-respostaFem.push(item)
-}
+/* respostaFem = []
+for (genero of gender) {
+if (genero === "Female") {
+respostaFem.push(genero)
+}}
 
 respostaMas = []
-for (item of genero) {
-  if (item === "Male")
-  respostaMas.push(item)
-}
-console.log(respostaMas);
-*/
+for (genero of gender) {
+  if (genero === "Male")
+  respostaMas.push(genero)
+} */
 
-/*export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};*/
-
-
-
-/*filterData(data, condition): esta função receberia os dados e nos retornaria os que cumprem com a condição.
-sortData(data, sortBy, sortOrder): esta função recebe três parâmetros. O primeiro, data, nos entrega os dados. O segundo, sortBy, diz respeito a qual das informações quer usar para ordenar. O terceiro, sortOrder, indica se quer ordenar de maneira crescente ou decrescente.
-computeStats(data): essa função nos permite fazer cálculos estatísticos básicos para serem exibidos de acordo com o que os dados permitem.  */
+//resposta3 = films.filter(genero => genero === "Male" || genero === "Female" )
--- src/main.js
+++ src/main.js
@@ -1,116 +1,134 @@
-import { orderGhibli, sortAZ, sortZA } from './data.js';
+import { sortAZ, sortZA, people, filtrar } from './data.js';
 
 import data from './data/ghibli/ghibli.js';
 
-console.log(orderGhibli)
+const personagens = data.films.filter(filmes => filmes.people); //usei map para pegar todos os títulos
+console.log(personagens)
 
-//const animation = data.films
-//console.log(orderGhibli, data);
-//console.log(genero, data);
-
+const cardCharacters = document.getElementById("cardCharacters")
+const card = document.getElementById("card")
 
 function printMovies(data) {
-  document.getElementById('card').innerHTML = data.map((film) => `
-  <div class="card">
-      <div class="front">
-      <div class="image" style="background-image: url(${film.poster})"></div>
-      <div class="text">
-      <h3 class="title">${film.title}</h3>
-      <h4 class="ano">${film.release_date} </h4>
-     </div> 
-    <div class="back">
-      <ul class="cardInfos">
-        <li><strong>Nome:</strong> ${film.title}</li>
-        <li><strong>Diretor: </strong>${film.director}</li>
-        <li><strong> Descrição:</strong> ${film.description}</li>
-      </ul>
-  </div>`)
+  document.getElementById('card').innerHTML = data.map((films) => `
+  <section class="container">
+      <div class="card-container">
+      <div class="card">
+          <figure class="front">
+            <img src="${films.poster}" class"img"> 
+            <div class="text">
+            <h3 class="title">${films.title}</h3>
+            <h4 class="ano">${films.release_date} </h4>
+          </figure> 
+        <figure class="back">
+          <ul class="cardInfos">
+            <li><strong>Title:</strong> ${films.title}</li>
+            <li><strong>Director: </strong>${films.director}</li>
+            <li class="textBack"><strong>Description: </strong> ${films.description}</li>
+          </ul>
+        </figure>
+        </div>
+      </div>    
+  </section>`).join(" ")
+  //evento.preventDefault();
 };
 
-printMovies(data.films)
+printMovies(data.films);
 
+var select = document.querySelector("select");
+select.addEventListener("change", ordenar);
 
+function ordenar(e) {
+  e.preventDefault()
+  cardCharacters.innerHTML = ""
+  var orderValues = select.value;
 
-function ordenarAZ() {
-  const ordered = printMovies(sortAZ(data.films))
-  return ordered
-}
-const selectOrdenar = document.getElementById("order");
-selectOrdenar.addEventListener("change", ordenarAZ);
+  if (orderValues === "az") {
+    const sortTitles = printMovies(sortAZ(data.films, orderValues));
+    console.log()
+    printMovies(sortTitles);
 
-function ordenarZA() {
-  const ordered2 = printMovies(sortZA(data.films))
-  return ordered2
-
-}
-const selectOrdenar2 = document.getElementById("za");
-selectOrdenar2.addEventListener("change", ordenarZA); 
-
-
-
-/*
-const animations = data.films
-const selectOrdenar = document.getElementById("order");
-
-function filterOrdenar(){
-    const valueSelected = selectOrdenar.value
-    const selectedMovies = orderGhibli(animations,valueSelected);
-    printMovies(selectedMovies);
+  } else if (orderValues === "za") {
+    const sortTitles = printMovies(sortZA(data.films, orderValues));
+    printMovies(sortTitles);
+  }
 }
 
+//const personagens = data.films.map(filmes => filmes.people); //usei map para pegar todos os títulos
+//console.log(personagens)
 
-selectOrdenar.addEventListener("change", filterOrdenar); */
+//Função para printar personagens e função filtrar
+
+function printCharacters(perso) {
+  document.getElementById('cardCharacters').innerHTML = perso.map((data) => `
+  <section class="container">
+      <div class="cardCharacters-container">
+      <div class="cardCharacters">
+          <figure class="front">
+            <img src="${data.img}" class"img"> 
+            <div class="text">
+            <h3 class="title">${data.name}</h3>
+            </figure> 
+        <figure class="back">
+          <ul class="cardInfos">
+          <div class="textBackCharacter">
+            <li><strong>Name: </strong> ${data.name}</li>
+            <li><strong>Age: </strong>${data.age}</li>
+            <li><strong>Eye color: </strong>${data.eye_color}</li>
+            <li><strong>Hair color: </strong>${data.hair_color}</li>
+            <li><strong>Gender: </strong>${data.gender}</li>
+            <li><strong>Specie: </strong>${data.specie}</li>
+            
+
+          </ul>
+        </figure>
+        </div> </div>
+      </div>    
+  </section>`).join(" ")
+
+}
+printCharacters(people);
 
 
-//const statisticInfo = document.getElementById("results");
+function filterCharacter(f) {
+  card.innerHTML = ""
+  const value = f.target.value;
+  if (value === "Male" || value === "Female") {
+    printCharacters(filtrar(people, "gender", value));
+  } else if (value === "Filters") {
+    printCharacters(people);
+  } else {
+    printCharacters(filtrar(people, "specie", value));
+  }
+}
+
+document.getElementById("genero").addEventListener("change", filterCharacter);
+
+//falta fazer pesquisa por busca 
+//const search = document.getElementById("search");
+
+//const animations = data.films.map(filmes => filmes.title); //usei map para pegar todos os títulos
+//console.log(animations)
 
 
-//declarei e atribui valor as variáveis 
-//const az = document.getElementById("az");
-/*const za = document.getElementById("za");
-const search = document.getElementById("search");
-const feminino = document.getElementById("feminino");
-const masculino = document.getElementById("masculino");*/
-
-
-/*az.onchange = function (evento) {
-  printMovies(data.films)
-  let crescente = sortAZ(data.title);
-  printMovies = (crescente);
-
-  evento.preventDefault();
-}; */
-
-
-/*const animations = data.films.map(filmes => filmes.title); //usei map para pegar todos os títulos
-console.log(animations)*/
-
-
-/*const respostaFem = data.films.filter(item => item.people)   //função que cria um novo array recebendo os item generos
-console.log(respostaFem)
-
-
-function ordenarAZ() {
+/*function ordenarAZ() {    //primeira tentativa de ordenar
   const filtroOrdenar = document.getElementById("order").value
   const ordenar = orderGhibli(data, animations)
   console.log(filtroOrdenar)
   document.getElementById("ordenar").addEventListener("change", ordenarAZ)
   return ordenarAZ
-}
+}*/
 
-*/
 
-/*window.onload = createCards(characters);
+/*function filtrar() {  // primeira tentativa de filtrar 
+  var orderValues2 = select.value;
 
-document.getElementById("cards").innerHTML = printCards;
+  if (orderValues2 === "feminino") {
+    const sortChars = printCharacters(filterData(data.films, orderValues2));
+    console.log()
+    printCharacters(sortChars);
 
-createCards(data.title);
-console.log("createCards")*/
-
-/*const ordenarAZ = (evento) => {
-
-    evento.preventDefault(); 
-    //chamando a função de ordenar AZ
-};*/
-
-//filterData(data, gender)
+  } else if (orderValues2 === "masculino") {
+    const sortChars = printCharacters(filterData(data.films, orderValues2));
+    printCharacters(sortChars);
+  }
+}; */
